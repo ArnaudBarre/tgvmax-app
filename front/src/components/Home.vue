b25a7a64c5f52b53987014f95a19a090800b79c2
arnaudbarre <arnaudbarremn@gmail.com>
2017-06-27 02:18:05 +0200

    put image source in :style for binding when build

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home py-5">
+  <div class="home py-5" :style="{'background-image': `url(${require('./../assets/tgv.jpg')})`}">
     <h1 class="text-white py-5">Envie de partir en weekend ?</h1>
     <h4 class="text-white py-5">Découvrez toutes les destinations possibles avec votre abonnement TGVmax !</h4>
     <router-link class="btn btn-primary btn-lg my-5" tag="button" to="/search">Rechercher</router-link>
@@ -8,7 +8,6 @@
 
 <style>
   .home {
-    background: url('./../assets/tgv.jpg') center;
     background-size: cover;
     height: 100vh;
     text-align: center;
