24175c13974229bee419dc492abeb8420a834bce
Arnaud Barré <arnaud.barre@kametventures.com>
2018-08-21 00:19:42 +0200

    few ES refecto

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-let express = require('express');
-let axios = require('axios');
-let compression = require('compression')
-let stations = require('./stations.json');
+const express = require('express');
+const axios = require('axios');
+const compression = require('compression')
 
 const api = 'https://ressources.data.sncf.com/api/records/1.0/search/?dataset=tgvmax';
 
-let app = express();
+const app = express();
 app.use(compression());
 app.use(express.static(__dirname + '/front'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
@@ -13,10 +12,10 @@ app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.get('/lastUpdate', (req, res) => {
   axios(api)
     .then(response => res.send(response.data.records[0].record_timestamp))
-    .catch(error => error.statusCode ? res.sendStatus(error.statusCode) : res.sendStatus(500));
+    .catch(({ statusCode }) => res.sendStatus(statusCode ? statusCode : 500));
 });
 
-let groupData = (data, key) => {
+const groupData = (data, key) => {
   return data.records
     .map(r => r.fields)
     .sort((a, b) => a.heure_depart.replace(":", "") - b.heure_depart.replace(":", ""))
@@ -27,7 +26,7 @@ let groupData = (data, key) => {
     }, {});
 };
 
-let callApi = (date, orgine, destination) => axios.get(api, {
+const callApi = (date, orgine, destination) => axios.get(api, {
   params: {
     rows: 1000,
     'refine.date': date,
@@ -41,16 +40,17 @@ app.get('/search', (req, res) => {
   Promise.all([
     callApi(req.query.startDate, req.query.from, null),
     callApi(req.query.endDate, null, req.query.from),
-  ]).then(responses => {
-    let dataGo = groupData(responses[0].data, 'destination')
-    let dataBack = groupData(responses[1].data, 'origine')
-    let commons = [];
+  ]).then(([startResponse, backResponse]) => {
+    const dataGo = groupData(startResponse.data, 'destination')
+    const dataBack = groupData(backResponse.data, 'origine')
+    const commons = [];
     Object.keys(dataGo).forEach(station => {
-      if (dataBack.hasOwnProperty(station))
+      if (dataBack.hasOwnProperty(station)) {
         commons.push({ station, go: [...dataGo[station]], back: [...dataBack[station]] })
+      }
     });
     res.send(commons);
-  }).catch(error => res.sendStatus(error.statusCode ? error.statusCode : 500));
+  }).catch(({ statusCode }) => res.sendStatus(statusCode ? statusCode : 500));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('App running at http://localhost:3000'));
