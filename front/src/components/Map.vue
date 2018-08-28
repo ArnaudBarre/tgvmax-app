56baf8d48c4e491b89db7832b2b544d7fd3c3d48
Arnaud Barré <arnaud.barre@kametventures.com>
2018-08-28 02:03:30 +0200

    remove zoomIn, add link in popup

--- front/src/components/Map.vue
+++ front/src/components/Map.vue
@@ -2,7 +2,7 @@
   <GmapMap :options="{ disableDefaultUI: true, minZoom: 6, maxZoom: 17 }"
            :center="center"
            :zoom="zoom"
-           @zoom_changed="$event => this.zoom = $event"
+           @zoom_changed="$event => set('zoom', $event)"
            map-type-id="roadmap"
            style="width: 100%; height: 100%">
     <GmapMarker :icon="stationIcon"
@@ -11,12 +11,17 @@
                 v-for="m in markers"
                 :position="m.coordinates"
                 :clickable="true"
-                @click="m.open = !m.open">
+                @click="m.popupOpen = !m.popupOpen">
       <GmapInfoWindow :options="{ maxWidth: 350 }"
                       :position="m.coordinates"
-                      :opened="m.open"
-                      @closeclick="m.open = false">
+                      :opened="m.popupOpen"
+                      @closeclick="m.popupOpen = false">
         <div>{{ m.name + ' - Fréquence : ' + Math.round(m.count / max * 100) + '%' }}</div>
+        <div class="text-xs-center">
+          <a @click="set('startStation', m.name)">Partir de là</a>
+          <span class="mx-2">-</span>
+          <a @click="set('endStation', m.name)">Y aller</a>
+        </div>
       </GmapInfoWindow>
     </GmapMarker>
     <GmapMarker v-if="userLocation" :icon="positionIcon" :position="userLocation"></GmapMarker>
@@ -26,20 +31,12 @@
 <script>
 import positionSVG from '../assets/location.svg';
 import trainSVG from '../assets/train.svg';
-import stations from '../../../stations';
 
 export default {
   data() {
     return {
-      markers: stations.map(marker => {
-        this.$set(marker, 'open', false);
-        return marker;
-      }),
-      max: Math.max(...stations.map(s => s.count)),
       positionIcon: { url: positionSVG, anchor: { x: 15, y: 15 } },
       stationIcon: { url: trainSVG, anchor: { x: 12, y: 12 } },
-      center: { lat: 46.5, lng: 2.5 },
-      zoom: 6,
     };
   },
   computed: {
@@ -51,22 +48,23 @@ export default {
     },
     endStation() {
       return this.$store.state.endStation;
+    },
+    max() {
+      return this.$store.state.max;
+    },
+    markers() {
+      return this.$store.state.stations;
+    },
+    center() {
+      return this.$store.state.center;
+    },
+    zoom() {
+      return this.$store.state.zoom;
     }, // TODO: spread getters
   },
   methods: {
-    stationSelected(value) {
-      if (!value) return;
-      this.center = this.markers.find(s => s.name === value).coordinates;
-      this.zoom = 11;
-      this.markers.find(m => m.name === value).open = true;
-    },
-  },
-  watch: {
-    startStation(value) {
-      this.stationSelected(value);
-    },
-    endStation(value) {
-      this.stationSelected(value);
+    set(key, value) {
+      this.$store.commit('set', { key, value });
     },
   },
 };
--- front/src/store.js
+++ front/src/store.js
@@ -1,3 +1,5 @@
+import stations from '../../stations';
+
 export default {
   state: {
     startStation: undefined,
@@ -6,10 +8,23 @@ export default {
     endDate: undefined,
     userLocation: undefined,
     results: undefined,
+    zoom: 5,
+    center: { lat: 46.5, lng: 2.5 },
+    stations: stations.map(s => {
+      s.popupOpen = false;
+      return s;
+    }),
+    max: Math.max(...stations.map(s => s.count)),
   },
   mutations: {
-    set(state, payload) {
-      state[payload.key] = payload.value;
-    }
-  }
+    set(state, { key, value }) {
+      state[key] = value;
+      if (key === 'startStation' || key === 'endStation') {
+        state.zoom = 5;
+        if (value) {
+          state.stations.find(s => s.name === value).popupOpen = true;
+        }
+      }
+    },
+  },
 };
